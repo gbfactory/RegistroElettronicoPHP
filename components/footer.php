9d5eab78ec08144849978f266d172874f151abc5
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-03-17 21:15:32 +0100

    Aggiunta sezione footer

--- components/footer.php
+++ components/footer.php
@@ -1,22 +1,45 @@
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text">INFORMAZIONI</h5>
+        <p class="grey-text text-lighten-4">Registro elettronico con sincronizzazione dati da Argo Scuola Next</p>
+      </div>
+      <div class="col l6 s12">
+        <h5 class="white-text">ATTENZIONE</h5>
+        <p class="grey-text text-lighten-4">Questo non è il registro ufficiale Argo, per utilizzare il registro ufficiale recarsi a questo link: <a href="https://www.argofamiglia.it/">https://www.argofamiglia.it/</a></p>
 
+      </div>
+
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      © 2020 GB Factory Scuola
+      <a class="grey-text text-lighten-4 right" href="https://gbfactory.net/">gbfactory.net</a>
+    </div>
+  </div>
+</footer>
 
 <script>
-    $(document).ready(function () {
-        $('.sidenav').sidenav();
-    });
+  $(document).ready(function() {
+    $('.sidenav').sidenav();
+  });
 </script>
 
 <script>
-  $(document).ready(function(){
+  $(document).ready(function() {
     $('.tabs').tabs();
   });
 </script>
 
 <script>
-$(document).ready(function(){
+  $(document).ready(function() {
     $('.tooltipped').tooltip();
   });
 </script>
+
+<script src="./assets/js/main.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
