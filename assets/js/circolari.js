50369fcaf3883afb6967196f60bf692b07a0a9b6
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-04-01 22:33:25 +0200

    Caricamento dinamico delle circolari

--- assets/js/circolari.js
+++ assets/js/circolari.js
@@ -1,10 +1,10 @@
-// =====================================
-// Caricamento circolari da Google Drive
-// =====================================
-
 $('#circolari').toggle();
 
 $(document).ready(function () {
+
+    // =====================================
+    // Caricamento circolari da Google Drive
+    // =====================================
 
     var api = 'https://www.googleapis.com/drive/v3/files?q="1t9aUapTaW69GwG2ug65odDKd3bd6LSG3"+in+parents&key=AIzaSyBpkmqMuIL9hUfs1p252dC0YvhyoG4Esyw';
 
@@ -24,29 +24,56 @@ $(document).ready(function () {
 
         $('#circolari').append(`
                             <li>
-                                <div class="collapsible-header"><i class="material-icons">folder_open</i>${apir.files[i].name}</div>
+                                <div class="collapsible-header" data-id="${folderId}"><i class="material-icons">folder_open</i>${apir.files[i].name}</div>
                                 <div class="collapsible-body">
                                     <ul class="collection" id="${folderId}"> </ul>
                                 </div>
                             </li>
                         `);
-
-        var api2 = 'https://www.googleapis.com/drive/v3/files?q="' + folderId + '"+in+parents&key=AIzaSyBpkmqMuIL9hUfs1p252dC0YvhyoG4Esyw';
-
-        $.ajax({
-            async: false,
-            url: api2,
-            success: function (rf) {
-                for (var i = 0; i < Object.keys(rf.files).length; i++) {
-                    $(`#${folderId}`).append(`<li class="collection-item"><a href="">${rf.files[i].name}</a></li>`)
-                }
-            }
-        })
-
     }
+
+    $('.collapsible-header').click(function() {
+        if ($(this).parents('.active').length == 0) {
+
+            var dataFolderId = $(this).attr('data-id');
+
+            if ($(`#${dataFolderId} > li`)[0]) {
+                return;  
+            }
+
+            //$(this).find('i').html('folder_open');
+
+            var dataApi = 'https://www.googleapis.com/drive/v3/files?q="' + dataFolderId + '"+in+parents&key=AIzaSyBpkmqMuIL9hUfs1p252dC0YvhyoG4Esyw';
+
+
+            $.ajax({
+                async: false,
+                url: dataApi,
+                success: function (rf) {
+                    for (var i = 0; i < Object.keys(rf.files).length; i++) {
+                        var modalUrl = 'https://drive.google.com/file/d/' + rf.files[i].id + '/preview';
+                        $(`#${dataFolderId}`).append(`<li class="collection-item"><a class="modal-trigger" href="${modalUrl}" target="_blank">${rf.files[i].name}</a></li>`)
+                    }
+                }
+            })
+
+        } /*else {
+            $(this).find('i').html('folder');
+        }*/
+ 
+    })
+
 
     $('#loader').toggle();
     $('#circolari').toggle();
 
-    $('.collapsible').collapsible();
+    //$('.modal').modal();
+    $('.collapsible').collapsible({
+        // onOpenStart() {
+        //     console.log(this);
+        //     console.log($(this));
+        // }
+    });
+
+
 });
