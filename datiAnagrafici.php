c47ce3c32f0f118d093e592f510edd40c511f951
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-04-28 01:16:43 +0200

    Dati visualizzati in una tabella & data leggibile

--- datiAnagrafici.php
+++ datiAnagrafici.php
@@ -1,81 +1,66 @@
-<?php include './components/header.php'; 
+<?php include './components/header.php';
 
 $request = $argo->schede();
 
 ?>
 <main>
 
-    <style>
-    .validate {
-        color: black !important;
-    }
-    </style>
-
    <div class="container">
-     <h3>Dati Anagrafici</h3>
+      <h3 class="header">Dati Anagrafici</h3>
+      <hr>
       <div class="row">
-         <form class="col s12">
-            <div class="row">
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desCognome"] ?> <?= $request[0]["alunno"]["desNome"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Alunno</label>
-               </div>
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["datNascita"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Data di Nascita</label>
-               </div>
-            </div>
-            <div class="row">
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["flgSesso"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Sesso</label>
-               </div>
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desCf"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Codice Fiscale</label>
-               </div>
-            </div>
-            <div class="row">
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desComuneNascita"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Comune di Nascita</label>
-               </div>
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desCittadinanza"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Cittadinanza</label>
-               </div>
-            </div>
-            <div class="row">
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desComuneResidenza"] ?> (<?= $request[0]["alunno"]["desCap"] ?>)" id="disabled" type="text" class="validate">
-                  <label for="disabled">Comune di Residenza</label>
-               </div>
-               <div class="input-field col s6">
-                <input disabled value="<?= $request[0]["alunno"]["desVia"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Via</label>
-               </div>
-            </div>
-            <div class="row">
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desComuneRecapito"] ?> (<?= $request[0]["alunno"]["desCapResidenza"] ?>)" id="disabled" type="text" class="validate">
-                  <label for="disabled">Comune di Recapito</label>
-               </div>
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desIndirizzoRecapito"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Via di Recapitop</label>
-               </div>
-            </div>
-            <div class="row">
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desTelefono"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Telefono</label>
-               </div>
-               <div class="input-field col s6">
-                  <input disabled value="<?= $request[0]["alunno"]["desCellulare"] ?>" id="disabled" type="text" class="validate">
-                  <label for="disabled">Cellulare</label>
-               </div>
-            </div>
-         </form>
+         <div class="col s12">
+            <table>
+               <tr>
+                  <td>Alunno</td>
+                  <td><?= $request[0]["alunno"]["desCognome"] ?> <?= $request[0]["alunno"]["desNome"] ?></td>
+               </tr>
+               <tr>
+                  <td>Data di Nascita</td>
+                  <td><?= dataLeggibile($request[0]["alunno"]["datNascita"]) ?></td>
+               </tr>
+               <tr>
+                  <td>Sesso</td>
+                  <td><?= $request[0]["alunno"]["flgSesso"] ?></td>
+               </tr>
+               <tr>
+                  <td>Codice Fiscale</td>
+                  <td><?= $request[0]["alunno"]["desCf"] ?></td>
+               </tr>
+               <tr>
+                  <td>Comune di Nascita</td>
+                  <td><?= $request[0]["alunno"]["desComuneNascita"] ?></td>
+               </tr>
+               <tr>
+                  <td>Cittadinanza</td>
+                  <td><?= $request[0]["alunno"]["desCittadinanza"] ?></td>
+               </tr>
+               <tr>
+                  <td>Comune di Residenza</td>
+                  <td><?= $request[0]["alunno"]["desComuneResidenza"] ?> (<?= $request[0]["alunno"]["desCap"] ?>)</td>
+               </tr>
+               <tr>
+                  <td>Via</td>
+                  <td><?= $request[0]["alunno"]["desVia"] ?></td>
+               </tr>
+               <tr>
+                  <td>Comune di Recapito</td>
+                  <td><?= $request[0]["alunno"]["desComuneRecapito"] ?> (<?= $request[0]["alunno"]["desCapResidenza"] ?>)</td>
+               </tr>
+               <tr>
+                  <td>Via di Recapito</td>
+                  <td><?= $request[0]["alunno"]["desIndirizzoRecapito"] ?></td>
+               </tr>
+               <tr>
+                  <td>Telefono</td>
+                  <td><?= $request[0]["alunno"]["desTelefono"] ?></td>
+               </tr>
+               <tr>
+                  <td>Cellulare</td>
+                  <td><?= $request[0]["alunno"]["desCellulare"] ?></td>
+               </tr>
+            </table>
+         </div>
       </div>
    </div>
 </main>
