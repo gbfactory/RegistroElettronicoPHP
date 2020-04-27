be7380bcb3e30e4196c9dc16636f30ee89fb443a
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-04-27 13:43:59 +0200

    Script spostati in "script.js"

--- components/footer.php
+++ components/footer.php
@@ -1,24 +1,7 @@
-<script>
-    $(document).ready(function() {
-        $('.sidenav').sidenav();
-    });
-</script>
+    <script src="./assets/js/script.js"></script>
 
-<script>
-    $(document).ready(function() {
-        $('.tabs').tabs();
-    });
-</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-<script>
-    $(document).ready(function() {
-        $('.tooltipped').tooltip();
-    });
-</script>
-
-<script src="./assets/js/main.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 
 </html>
