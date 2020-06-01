a3fff7911a62f559c56b2cf2ad7a54a7a77be294
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-06-01 16:24:23 +0200

    Rimossa funzione già presente in header.php

--- compitiAssegnati.php
+++ compitiAssegnati.php
@@ -2,15 +2,10 @@
 
 $compiti = $argo->compiti();
 
-// https://stackoverflow.com/questions/5341168/best-way-to-make-links-clickable-in-block-of-text
-function make_links_clickable($text){
-    return preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $text);
-}
-
 ?>
 <main>
-
     <div class="container">
+
         <h3 class="header">Compiti Assegnati</h3>
 
         <hr>
@@ -18,7 +13,7 @@ function make_links_clickable($text){
         <div class="row">
             <div class="col s12">
                 <ul class="collection">
-                <?php for ($x = 0; $x < count($compiti); $x++) { ?>
+                    <?php for ($x = 0; $x < count($compiti); $x++) { ?>
                         <li class="collection-item avatar">
 
                             <?php
@@ -41,12 +36,8 @@ function make_links_clickable($text){
                             <span class="title"><?= $compiti[$x]['desMateria'] ?></span>
                                 <?php
 
-                                    // Data
-                                    $datCompitiSplit = explode('-', $compiti[$x]['datCompiti']);
-                                    $datCompiti = $datCompitiSplit[2] . '/' . $datCompitiSplit[1] . '/' . $datCompitiSplit[0];
-
-                                    $datGiornoSplit = explode('-', $compiti[$x]['datGiorno']);
-                                    $datGiorno = $datGiornoSplit[2] . '/' . $datGiornoSplit[1] . '/' . $datCompitiSplit[0];
+                                    $datCompiti = dataLeggibile($compiti[$x]['datCompiti']);
+                                    $datGiorno = dataLeggibile($compiti[$x]['datGiorno']);
 
                                     if ($compiti[$x]['datGiorno'] == $compiti[$x]['datCompiti']) {
                                         echo('<p>Assegnati il <b>' . $datGiorno . '</b></p>');
@@ -55,7 +46,7 @@ function make_links_clickable($text){
                                     }
                                 ?>
                             <p>
-                                <?= make_links_clickable($compiti[$x]['desCompiti']) ?> <br> <?= $compiti[$x]['docente'] ?>
+                                <?= linkCliccabili($compiti[$x]['desCompiti']) ?> <br> <?= $compiti[$x]['docente'] ?>
                             </p>
                         </li>
                     <?php } ?>
@@ -65,7 +56,5 @@ function make_links_clickable($text){
 
     </div>
 </main>
-
-
 
 <?php include './components/footer.php'; ?>
