ac885883d7460d1fdf9932ec3a4c3bbcf058ef1d
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-06-01 16:25:50 +0200

    Aggiunte frecce link nelle schede eventi

--- home.php
+++ home.php
@@ -1,10 +1,7 @@
 <?php include './components/header.php';
 
-//$riepilogo = $argo->oggiScuola('2020-01-31'); 
-
 if (isset($_GET['date'])) {
     $date = $_GET['date'];
-
 
     $datejs = explode('-', $date);
     $datejsanno = $datejs[0];
@@ -26,146 +23,161 @@ $riepilogo = $argo->oggiScuola($date);
 <main>
     <div class="container">
 
-        <?php //print( '<pre> ' . print_r($riepilogo, true) . '</pre>') 
-        ?>
-
         <h3 class="header">Riepilogo giornaliero</h3>
 
         <h6>
-        <a class="right-align btn-floating waves-effect waves-light red datepicker"><i class="material-icons">date_range</i></a>
-        <?php
+            <a class="right-align btn-floating waves-effect waves-light red datepicker"><i class="material-icons">date_range</i></a>
+            <?php
 
-            if (isset($_GET['date'])) {
-                echo "Riepilogo del " . dataLeggibile($_GET['date']);
-            } else {
-                // Codice date da https://www.mrwebmaster.it/php/data-oggi-italiano_11815.html
+                if (isset($_GET['date'])) {
+                    echo "Riepilogo del " . dataLeggibile($_GET['date']);
+                } else {
+                    // https://www.mrwebmaster.it/php/data-oggi-italiano_11815.html
 
-                // definisco due array arbitrarie
-                $giorni = array("Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato");
-                $mesi = array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre");
+                    $giorni = array("Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato");
+                    $mesi = array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre");
 
-                // giorno della settimana in italiano
-                $numero_giorno_settimana = date("w");
-                $nome_giorno = $giorni[$numero_giorno_settimana];
+                    $nome_giorno = $giorni[date("w")]; // giorno della settimana in italiano
+                    $numero_giorno_mese = date("j"); // giorno del mese
+                    $nome_mese = $mesi[date("n") - 1]; // nome del mese in italiano
+                    $anno = date("Y"); // numero dell'anno
 
-                // giorno del mese
-                $numero_giorno_mese = date("j");
-
-                // nome del mese in italiano
-                $numero_mese = date("n");
-                $nome_mese = $mesi[$numero_mese - 1];
-
-                // numero dell'anno
-                $anno = date("Y");
-
-                // Stampo a video
-                echo "Oggi è " . $nome_giorno . " " . $numero_giorno_mese . " " . $nome_mese . " " . $anno;
-            }
-        ?>
+                    // Stampo a video
+                    echo "Oggi è " . $nome_giorno . " " . $numero_giorno_mese . " " . $nome_mese . " " . $anno;
+                }
+            ?>
         </h6>
-
-        
 
         <hr>
 
-        <?php for ($x = 0; $x < count($riepilogo); $x++) { ?>
-            <div class="promemoria-header">
-                <p class="riepilogo-titolo"><b><?php echo $riepilogo[$x]['titolo'] ?></b></p>
-                <blockquote>
-                    <?php
+        <?php
+        for ($x = 0; $x < count($riepilogo); $x++) {
+            $tipo = $riepilogo[$x]['tipo']; ?>
+                
+            <?php // Promemoria
+            if ($tipo == 'PRO') { ?>
+                <div class="promemoria-header">
+                    <p class="riepilogo-titolo valign-wrapper">
+                        <b>Promemoria</b>
+                        <a href="promemoria.php"><i class="material-icons">chevron_right</i></a>
+                    </p>
+                    <blockquote>
+                        <?= linkCliccabili($riepilogo[$x]['dati']['desAnnotazioni']) ?> <br>
+                        <i><?= $riepilogo[$x]['dati']['desMittente'] ?></i>
+                    </blockquote>
+                </div>
 
-                    $tipo = $riepilogo[$x]['tipo'];
+            <?php // Compiti assegnati
+            } else if ($tipo == 'COM') { ?>
+                <div class="promemoria-header">
+                    <p class="riepilogo-titolo valign-wrapper">
+                        <b>Compiti Assegnati</b>
+                        <a href="compiti.php"><i class="material-icons">chevron_right</i></a>
+                    </p>
+                    <blockquote>
+                        <b><?= $riepilogo[$x]['dati']['desMateria'] ?></b> <br>
+                        <?= linkCliccabili($riepilogo[$x]['dati']['desCompiti']) ?> <br>
+                        <i><?= $riepilogo[$x]['dati']['docente'] ?></i>
+                    </blockquote>
+                </div>
 
-                    // Promemoria
-                    if ($tipo == 'PRO') {
+            <?php // Argomenti lezione
+            } else if ($tipo == 'ARG') { ?>
+                <div class="promemoria-header">
+                    <p class="riepilogo-titolo valign-wrapper">
+                        <b>Argomenti Lezione</b>
+                        <a href="argomenti.php"><i class="material-icons">chevron_right</i></a>
+                    </p>
+                    <blockquote>
+                        <b><?= $riepilogo[$x]['dati']['desMateria'] ?></b> <br>
+                        <?= linkCliccabili($riepilogo[$x]['dati']['desArgomento']) ?> <br>
+                        <i><?= $riepilogo[$x]['dati']['docente'] ?></i>
+                    </blockquote>
+                </div>
 
-                        echo linkCliccabili($riepilogo[$x]['dati']['desAnnotazioni']) . '<br>';
-                        echo '<i>' . $riepilogo[$x]['dati']['desMittente'] . '</i>';
+            <?php // Voto
+            } else if ($tipo == 'VOT') {
+                
+                $codProva = $riepilogo[$x]['dati']['codVotoPratico'];
+                $tipProva = '';
+                if ($codProva == 'S') {
+                    $tipProva = 'Scritto';
+                } else if ($codProva == 'N') {
+                    $tipProva == 'Orale';
+                } else if ($codProva == 'P') {
+                    $tipProva == 'Pratico';
+                }
 
-                        // Compiti assegnati
-                    } else if ($tipo == 'COM') {
+                $voto = $riepilogo[$x]['dati']['decValore'];
+                if ($voto <= 1) {
+                    $vColor = 'red darken-4';
+                } else if (($voto >= 1) && ($voto < 5)) {
+                    $vColor = 'red';
+                } else if (($voto >= 5) && ($voto < 6)) {
+                    $vColor = 'orange darken-4';
+                } else if (($voto >= 6) && ($voto < 7)) {
+                    $vColor = 'lime';
+                } else if (($voto >= 7) && ($voto < 8)) {
+                    $vColor = 'lime darken-2';
+                } else if (($voto >= 8) && ($voto < 9)) {
+                    $vColor = 'light-green';
+                } else if (($voto >= 9) && ($voto < 10)) {
+                    $vColor = 'green';
+                } else if ($voto >= 10) {
+                    $vColor = 'green darken-2';
+                }
+            ?>
+                <div class="promemoria-header">
+                    <p class="riepilogo-titolo valign-wrapper">
+                        <b>Voti Giornalieri</b>
+                        <a href="voti.php"><i class="material-icons">chevron_right</i></a>
+                    </p>
+                    <blockquote>
+                        <a class="btn-floating <?= $vColor ?>">
+                            <i><?= $riepilogo[$x]['dati']['codVoto'] ?></i>
+                        </a>
+                        <span>
+                            <b><?= $riepilogo[$x]['dati']['desMateria'] ?></b> <br>
 
-                        echo '<b>' . $riepilogo[$x]['dati']['desMateria'] . '</b> <br>';
-                        echo linkCliccabili($riepilogo[$x]['dati']['desCompiti']) . '<br>';
-                        echo '<i>' . $riepilogo[$x]['dati']['docente'] . '</i>';
+                            <?php if ($riepilogo[$x]['dati']['desProva']) { ?>
+                                <b>Descrizione:</b> <?= $riepilogo[$x]['dati']['desProva'] ?> <br>
+                            <?php } ?>
 
-                        // Argomenti lezione
-                    } else if ($tipo == 'ARG') {
+                            <?php if ($riepilogo[$x]['dati']['desCommento']) { ?>
+                                <b>Commento:</b> <?= $riepilogo[$x]['dati']['desCommento'] ?> <br>
+                            <?php } ?>
 
-                        echo '<b>' . $riepilogo[$x]['dati']['desMateria'] . '</b> <br>';
-                        echo linkCliccabili($riepilogo[$x]['dati']['desArgomento']) . '<br>';
-                        echo '<i>' . $riepilogo[$x]['dati']['docente'] . '</i>';
+                            <i><?= $riepilogo[$x]['dati']['docente'] ?></i>
+                        </span>
+                    </blockquote>
+                </div>
+                
+            <?php // Bacheca    
+            } else if ($tipo == 'BAC') { ?>
+                <div class="promemoria-header">
+                    <p class="riepilogo-titolo valign-wrapper">
+                        <b>Bacheca</b>
+                        <a href="bacheca.php"><i class="material-icons">chevron_right</i></a>
+                    </p>
+                    <blockquote>
+                        <b><?= $riepilogo[$x]['dati']['desOggetto'] ?></b> <br>
+                        <?= $riepilogo[$x]['dati']['desMessaggio'] ?>
+                    </blockquote>
+                </div>
 
-                        // Voto
-                    } else if ($tipo == 'VOT') {
-
-
-                        $codProva = $riepilogo[$x]['dati']['codVotoPratico'];
-                        if ($codProva == 'S') {
-                            $tipProva = 'Scritto';
-                        } else if ($codProva == 'N') {
-                            $tipProva == 'Orale';
-                        } else if ($codProva == 'P') {
-                            $tipProva == 'Pratico';
-                        }
-
-                        $voto = $riepilogo[$x]['dati']['decValore'];
-                        if ($voto <= 1) {
-                            $vColor = 'red darken-4';
-                        } else if (($voto >= 1) && ($voto < 5)) {
-                            $vColor = 'red';
-                        } else if (($voto >= 5) && ($voto < 6)) {
-                            $vColor = 'orange darken-4';
-                        } else if (($voto >= 6) && ($voto < 7)) {
-                            $vColor = 'lime';
-                        } else if (($voto >= 7) && ($voto < 8)) {
-                            $vColor = 'lime darken-2';
-                        } else if (($voto >= 8) && ($voto < 9)) {
-                            $vColor = 'light-green';
-                        } else if (($voto >= 9) && ($voto < 10)) {
-                            $vColor = 'green';
-                        } else if ($voto >= 10) {
-                            $vColor = 'green darken-2';
-                        }
-                    ?>
-
-                        <div class="row valign-wrapper">
-                            <div class="col s1">
-                                <?php echo '<i class="circle ' . $vColor . '">' . $riepilogo[$x]['dati']['codVoto'] . '</i>';  ?>
-                            </div>
-                            <div class="col s12">
-                                <span class="black-text">
-                                    <?php
-                                    echo '<b>' . $riepilogo[$x]['dati']['desMateria'] . '</b> <br>';
-
-                                    if ($voti[$x]['desProva'] != '') {
-                                        echo $riepilogo[$x]['dati']['desProva'] . '<br>';
-                                    }
-
-                                    if ($voti[$x]['desCommento'] != '') {
-                                        echo $riepilogo[$x]['dati']['desCommento'] . '<br>';
-                                    }
-
-                                    echo '<i>' . $riepilogo[$x]['dati']['docente'] . '</i>';
-                                    ?>
-                                </span>
-                            </div>
-                        </div>
-                    <?php
-
-                        // Bacheca    
-                    } else if ($tipo == 'BAC') {
-                        echo '<b>' . $riepilogo[$x]['dati']['desOggetto'] . '</b> <br>';
-                        echo $riepilogo[$x]['dati']['desMessaggio'];
-
-                        // Note
-                    } else if ($tipo == 'NOT') {
-                        echo $riepilogo[$x]['dati']['desNota'] . '<br>';
-                        echo '<i>' . $riepilogo[$x]['dati']['docente'] . '</i>';
-                    }  ?>
-
-                </blockquote>
-            </div>
+            <?php // Note
+            } else if ($tipo == 'NOT') { ?>
+                <div class="promemoria-header">
+                    <p class="riepilogo-titolo valign-wrapper">
+                        <b>Note Disciplinari</b>
+                        <a href="note.php"><i class="material-icons">chevron_right</i></a>
+                    </p>
+                    <blockquote>
+                        <?= $riepilogo[$x]['dati']['desNota'] ?> <br>
+                        <i><?= $riepilogo[$x]['dati']['docente'] ?></i>
+                    </blockquote>
+                </div>
+            <?php } ?>
         <?php } ?>
 
         <script>
