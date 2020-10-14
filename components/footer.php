55ff37d9a73d25a32f722b2702bcefd32dcafddc
GB Factory <37420179+gbfactory@users.noreply.github.com>
2020-10-14 18:00:37 +0200

    📈 Aggiunto Google Analytics

--- components/footer.php
+++ components/footer.php
@@ -2,6 +2,8 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
+    <?php include './components/analyticstracking.php' ?>
+
 </body>
 
 </html>
