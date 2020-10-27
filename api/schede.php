6f2be7a56fb2f7547475b71f23001229b7326814
GB Factory <37420179+gbfactory@users.noreply.github.com>
2020-10-27 20:03:44 +0100

    👽 Modifica header api

--- api/schede.php
+++ api/schede.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: *');
 header("Content-Type: application/json; charset=UTF-8");
 
 $getHeaders = getallheaders();
