ff816631fa8e05c8b1d6118987c5f09851c821e3
GB Factory <37420179+gbfactory@users.noreply.github.com>
2020-11-05 10:36:49 +0100

    🚚 Divisi voti giornalieri e scrutinio

--- scrutinio.php
+++ scrutinio.php
@@ -8,44 +8,11 @@ $scrutinio = $argo->votiScrutinio();
 
     <div class="container">
 
-        <h3 class="header">Valutazioni</h3>
+        <h3 class="header">Voti Scrutinio</h3>
 
         <hr>
 
         <div class="row">
-
-            <div class="col s12" style="margin-bottom: 1rem;">
-                <ul class="tabs">
-                    <li class="tab col s4"><a href="voti.php">RIEPILOGO</a></li>
-                    <li class="tab col s4"><a href="valutazioni.php">VALUTAZIONI</a></li>
-                    <li class="tab col s4"><a class="active" href="scrutinio.php">SCRUTINIO</a></li>
-                </ul>
-            </div>
-
-            <?php
-
-            function coloreVoto($voto) {
-                if ($voto <= 1) {
-                    return 'red darken-4';
-                } else if (($voto >= 1) && ($voto < 5)) {
-                    return 'red';
-                } else if (($voto >= 5) && ($voto < 6)) {
-                    return 'orange darken-4';
-                } else if (($voto >= 6) && ($voto < 7)) {
-                    return 'lime';
-                } else if (($voto >= 7) && ($voto < 8)) {
-                    return 'lime darken-2';
-                } else if (($voto >= 8) && ($voto < 9)) {
-                    return 'light-green';
-                } else if (($voto >= 9) && ($voto < 10)) {
-                    return 'green';
-                } else if ($voto >= 10) {
-                    return 'green darken-2';
-                }
-            }
-
-            ?>
-
             <div id="scrutinio" class="col s12">
                 <div class="row">
                     <div class="col s6">
--- voti.php
+++ voti.php
@@ -1,124 +1,75 @@
 <?php include './components/header.php';
 
 $voti = $argo->votiGiornalieri();
+$scrutinio = $argo->votiScrutinio();
 $schede = $argo->schede();
 
 ?>
 <main>
-
     <div class="container">
 
-        <h3 class="header">Valutazioni</h3>
+        <h3 class="header">Voti Giornalieri</h3>
 
         <hr>
+
+        <?php
+        $materie = [];
+
+        $votiTotSomma = 0;
+        $votiTriSomma = 0;
+        $votiPenSomma = 0;
+
+        $votiTotCount = count($voti);
+        $votiTriCount = 0;
+        $votiPenCount = 0;
+
+        $annoInizio = $schede[0]['annoScolastico']['datInizio'];
+        $annoFine = $schede[0]['annoScolastico']['datFine'];
+
+        for ($x = 0; $x < count($voti); $x++) {
+
+            $materia = $voti[$x]['desMateria'];
+            $voto = $voti[$x]['decValore'];
+            $data = strtotime($voti[$x]['datGiorno']);
+
+            // Array materie
+            if (!in_array($materia, $materie)) {
+                array_push($materie, $materia);
+            }
+
+            if ($voto != 0) {
+
+                // Media totale
+                $votiTotSomma += $voto;
+
+                // Media trimetre
+                $dataTri = strtotime(explode('-', $annoInizio)[0] . '-12-31');
+                $dataPen = strtotime(explode('-', $annoFine)[0] . '-01-01');
+
+                if ($data <= $dataTri) {
+                    $votiTriSomma += $voto;
+                    $votiTriCount++;
+                } else if ($data >= $dataPen) {
+                    $votiPenSomma += $voto;
+                    $votiPenCount++;
+                }
+            }
+        }
+
+        $mediaTot = $votiTotCount > 0 ? round($votiTotSomma / $votiTotCount, 2) : 0;
+        $mediaTri = $votiTriCount > 0 ? round($votiTriSomma / $votiTriCount, 2) : 0;
+        $mediaPen = $votiPenCount > 0 ? round($votiPenSomma / $votiPenCount, 2) : 0;
+        ?>
 
         <div class="row">
 
             <div class="col s12" style="margin-bottom: 1rem;">
                 <ul class="tabs">
-                    <li class="tab col s4"><a class="active" href="voti.php">RIEPILOGO</a></li>
-                    <li class="tab col s4"><a href="valutazioni.php">VALUTAZIONI</a></li>
-                    <li class="tab col s4"><a href="scrutinio.php">SCRUTINIO</a></li>
+                    <li class="tab col s6"><a class="active" href="#riepilogo">RIEPILOGO</a></li>
+                    <li class="tab col s6"><a href="#valutazioni">VALUTAZIONI</a></li>
                 </ul>
             </div>
 
-            <?php
-
-            function coloreVoto($voto) {
-                if ($voto <= 1) {
-                    return 'red darken-4';
-                } else if (($voto >= 1) && ($voto < 5)) {
-                    return 'red';
-                } else if (($voto >= 5) && ($voto < 6)) {
-                    return 'orange darken-4';
-                } else if (($voto >= 6) && ($voto < 7)) {
-                    return 'lime';
-                } else if (($voto >= 7) && ($voto < 8)) {
-                    return 'lime darken-2';
-                } else if (($voto >= 8) && ($voto < 9)) {
-                    return 'light-green';
-                } else if (($voto >= 9) && ($voto < 10)) {
-                    return 'green';
-                } else if ($voto >= 10) {
-                    return 'green darken-2';
-                }
-            }
-
-            function coloreGrafico($voto) {
-                if ($voto <= 1) {
-                    return '183, 28, 28';
-                } else if (($voto >= 1) && ($voto < 5)) {
-                    return '244, 67, 54';
-                } else if (($voto >= 5) && ($voto < 6)) {
-                    return '230, 81, 0';
-                } else if (($voto >= 6) && ($voto < 7)) {
-                    return '205, 220, 57';
-                } else if (($voto >= 7) && ($voto < 8)) {
-                    return '175, 180, 43';
-                } else if (($voto >= 8) && ($voto < 9)) {
-                    return '139, 195, 74';
-                } else if (($voto >= 9) && ($voto < 10)) {
-                    return '76, 175, 80';
-                } else if ($voto >= 10) {
-                    return '56, 142, 60';
-                }
-            }
-
-            ?>
-
-            <?php
-            $materie = [];
-
-            $votiTotSomma = 0;
-            $votiTriSomma = 0;
-            $votiPenSomma = 0;
-
-            $votiTotCount = count($voti);
-            $votiTriCount = 0;
-            $votiPenCount = 0;
-
-            $annoInizio = $schede[0]['annoScolastico']['datInizio'];
-            $annoFine = $schede[0]['annoScolastico']['datFine'];
-
-            for ($x = 0; $x < count($voti); $x++) {
-
-                $materia = $voti[$x]['desMateria'];
-                $voto = $voti[$x]['decValore'];
-                $data = strtotime($voti[$x]['datGiorno']);
-
-                // Array materie
-                if (!in_array($materia, $materie)) {
-                    array_push($materie, $materia);
-                }
-
-                if ($voto != 0) {
-
-                    // Media totale
-                    $votiTotSomma += $voto;
-
-                    // Media trimetre
-                    $dataTri = strtotime(explode('-', $annoInizio)[0] . '-12-31');
-                    $dataPen = strtotime(explode('-', $annoFine)[0] . '-01-01');
-
-                    if ($data <= $dataTri) {
-                        $votiTriSomma += $voto;
-                        $votiTriCount++;
-                    } else if ($data >= $dataPen) {
-                        $votiPenSomma += $voto;
-                        $votiPenCount++;
-                    }
-                }
-            }
-
-            $mediaTot = $votiTotCount > 0 ? round($votiTotSomma / $votiTotCount, 2) : 0;
-            $mediaTri = $votiTriCount > 0 ? round($votiTriSomma / $votiTriCount, 2) : 0;
-            $mediaPen = $votiPenCount > 0 ? round($votiPenSomma / $votiPenCount, 2) : 0;
-
-            ?>
-
-            <!--===================================
-                RIEPILOGO VOTI
-                ===================================-->
             <div id="riepilogo" class="col s12">
 
                 <div class="row">
@@ -180,7 +131,6 @@ $schede = $argo->schede();
                                     array_push(${"listaVoti2" . $i}, $voti[$j]);
                                 }
                             }
-
                         }
                     }
                 }
@@ -405,9 +355,35 @@ $schede = $argo->schede();
                 </script>
 
             </div>
-        </div>
 
+            <div id="valutazioni" class="col s12">
+                <ul class="collection">
+                    <?php for ($x = 0; $x < count($voti); $x++) { ?>
+                        <li class="collection-item avatar">
+                            <i class="circle <?= coloreVoto($voti[$x]['decValore']) ?>"><?= $voti[$x]['codVoto'] ?></i>
+
+                            <span class="title"><?= $voti[$x]['desMateria'] ?>
+                                <?php if (substr($voti[$x]['desCommento'], -14) == '(non fa media)') echo '<b>(Non fa media)</b>'; ?>
+                            </span>
+
+                            <p><?= dataLeggibile($voti[$x]['datGiorno']) ?> - <?= tipoProva($codProva = $voti[$x]['codVotoPratico']) ?></p>
+
+                            <p><?php if ($voti[$x]['desProva'] != '') echo ('<b>Descrizione:</b> ' . $voti[$x]['desProva']); ?>
+                            <p><?php if ($voti[$x]['desCommento'] != '') echo ('<b>Commento:</b> ' . $voti[$x]['desCommento']); ?>
+                            <p><i><?= rimuovi_parentesi($voti[$x]['docente']); ?></i></p>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+
+        </div>
     </div>
 </main>
+
+<script>
+    $(document).ready(function() {
+        $('.tabs').tabs();
+    });
+</script>
 
 <?php include './components/footer.php'; ?>
