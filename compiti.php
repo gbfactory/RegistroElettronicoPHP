40581b0618a6e2dd9ef7b6bd6863992f3a1a9ac6
GB Factory <37420179+gbfactory@users.noreply.github.com>
2020-11-17 14:10:40 +0100

    ✨ Aggiunto filtro materie

--- compiti.php
+++ compiti.php
@@ -10,15 +10,38 @@ $compiti = $argo->compiti();
 
         <hr>
 
+        <?php
+        $materie = [];
+        for ($x = 0; $x < count($compiti); $x++) {
+            if (!in_array($compiti[$x]['desMateria'], $materie)) {
+                array_push($materie, $compiti[$x]['desMateria']);
+            }
+        }
+        ?>
+
+        <style>
+            .chip {
+                padding: 5px 12px;
+            }
+        </style>
+
         <div class="row">
             <div class="col s12">
+                <?php for ($x = 0; $x < count($materie); $x++) { ?>
+                    <div class="chip">
+                        <label>
+                            <input type="checkbox" class="filled-in" checked="checked" value="<?= str_replace(' ', '', $materie[$x]) ?>" />
+                            <span><?= $materie[$x] ?></span>
+                        </label>
+                    </div>
+                <?php } ?>
                 <ul class="collection">
                     <?php
                     for ($x = 0; $x < count($compiti); $x++) {
                         $datCompiti = $compiti[$x]['datCompiti'];
                         $datGiorno = $compiti[$x]['datGiorno'];
                         ?>
-                        <li class="collection-item avatar">
+                        <li class="collection-item avatar" data-id="<?= str_replace(' ', '', $compiti[$x]['desMateria']) ?>">
                             <i class="material-icons circle <?= colore_data($datCompiti) ?>">book</i>
                             <span class="title"><?= $compiti[$x]['desMateria'] ?></span>
                             <p>Assegnati il <b><?= dataLeggibile($datGiorno) ?></b> per il <b><?= dataLeggibile($datCompiti) ?></b></p>
@@ -29,6 +52,15 @@ $compiti = $argo->compiti();
                 </ul>
             </div>
         </div>
+
+        <script>
+            $(document).ready(function(){
+                $('input[type="checkbox"]').click(function(){
+                    var inputValue = $(this).attr("value");
+                    $("[data-id='" + inputValue + "']").toggle();
+                });
+            });
+        </script>
 
     </div>
 </main>
