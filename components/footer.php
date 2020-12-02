c393cd3cf0a75cbf2ef81ed894c8020b82a49ad4
GB Factory <37420179+gbfactory@users.noreply.github.com>
2020-12-02 11:50:05 +0100

    ⛏ Spostato script materializecss

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,6 @@
-    <script src="./assets/js/script.js"></script>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script src="./assets/js/script.js"></script>
 
     <?php include './components/analyticstracking.php' ?>
 
