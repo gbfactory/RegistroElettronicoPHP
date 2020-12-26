0ac4dba793c7035f82023607ca4e8bbebd6e4e8d
GB Factory <37420179+gbfactory@users.noreply.github.com>
2020-12-26 19:12:29 +0100

    ✨ UI Ricerca e Not found

--- argomenti.php
+++ argomenti.php
@@ -13,20 +13,35 @@ $argomenti = $argo->argomenti();
         <div class="row">
             <div class="col s12">
 
-                <input type="text" id="argCerca" placeholder="Cerca argomento...">
+                <?php if (empty($argomenti)) { ?>
+                    <div class="center">
+                        <h5>Non ci sono argomenti!</h5>
+                    </div>
+                <?php } else { ?>
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="input-field">
+                                <i class="material-icons prefix">search</i>
+                                <input type="text" id="argCerca">
+                                <label for="argCerca">Cerca argomento...</label>
+                            </div>
+                        </div>
+                    </div>
 
-                <ul class="collection">
-                    <?php for ($x = 0; $x < count($argomenti); $x++) { ?>
-                        <li class="collection-item avatar">
-                            <i class="material-icons circle blue darken-2">reorder</i>
+                    <ul class="collection">
+                        <?php for ($x = 0; $x < count($argomenti); $x++) { ?>
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle blue darken-2">reorder</i>
 
-                            <span class="title"><b><?= $argomenti[$x]['desMateria'] ?> - <?= dataLeggibile($argomenti[$x]['datGiorno']) ?></b></span>
+                                <span class="title"><b><?= $argomenti[$x]['desMateria'] ?> - <?= dataLeggibile($argomenti[$x]['datGiorno']) ?></b></span>
 
-                            <p><?= linkCliccabili($argomenti[$x]['desArgomento']) ?></p>
+                                <p><?= linkCliccabili($argomenti[$x]['desArgomento']) ?></p>
 
-                        </li>
-                    <?php } ?>
-                </ul>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                <?php } ?>
+
             </div>
         </div>
 
