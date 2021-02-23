27453d7dbcb2b8e672d3df6f266e897676c1e36f
GB Factory <37420179+gbfactory@users.noreply.github.com>
2021-02-23 02:13:07 +0100

    🐛 Fix errore file non trovato

--- bacheca.php
+++ bacheca.php
@@ -28,7 +28,7 @@ $bacheca = $argo->bacheca();
 
                                 <?php if (isset($bacheca[$x]['allegati'][0])) {
                                     for($i = 0; $i < count($bacheca[$x]['allegati']); $i++) { ?>
-                                        <p><b>Allegato:</b> <a href="<?= "http://www.portaleargo.it/famiglia/api/rest/messaggiobachecanuova?id=FFF" . $codice . "EEEII000010000000" . str_pad($bacheca[$x]['allegati'][$i]['prgMessaggio'], 3 ,"0", STR_PAD_LEFT) . str_replace('-', '', $token) . "ax6542sdru3217t4eesd9"; ?>">
+                                        <p><b>Allegato:</b> <a href="<?= "http://www.portaleargo.it/famiglia/api/rest/messaggiobachecanuova?id=FFF" . $codice . "EEEII00001" . str_pad($bacheca[$x]['allegati'][$i]['prgMessaggio'], 10 ,"0", STR_PAD_LEFT) . str_replace('-', '', $token) . "ax6542sdru3217t4eesd9"; ?>">
                                         <?= $bacheca[$x]['allegati'][$i]['desFile'] ?></a></p>
                                 <?php }
                                 } ?>
@@ -83,7 +83,7 @@ $bacheca = $argo->bacheca();
 
 <script>
     function presaVisione(id) {
-        console.log(id);
+        // console.log(id);
 
         $.ajax({
             type: "POST",
@@ -93,10 +93,10 @@ $bacheca = $argo->bacheca();
                 "x-key-app": "ax6542sdru3217t4eesd9",
                 "x-app-code": "APF",
                 "x-prg-scheda": 1,
-                "x-prg-alunno": <?= intval($codAlunno) ?>,
+                "x-prg-alunno": <?= intval($codAlunno); ?>,
                 "x-auth-token": "<?= $token; ?>",
                 "x-produttore-software": "ARGO Software s.r.l. - Ragusa",
-                "x-cod-min": "Sg26685",
+                "x-cod-min": "<?= $codice; ?>",
                 "x-prg-scuola": 1,
             },
             data: JSON.stringify({
