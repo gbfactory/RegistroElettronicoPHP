5e3e85bdb1ea03aa660d03ecb422836c2468879c
GB Factory <37420179+gbfactory@users.noreply.github.com>
2021-05-23 22:43:37 +0200

    ✨ Aggiunto footer

--- components/footer.php
+++ components/footer.php
@@ -1,8 +1,16 @@
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<footer class="page-footer">
+    <div class="footer-copyright">
+        <div class="container">
+            <p><b>&copy; 2021 GB Factory</b> - Sito web realizzato da <a href="https://giacomobrochin.com/" class="grey-text text-lighten-4">Giacomo Brochin</a></p>
+        </div>
+    </div>
+</footer>
 
-    <script src="./assets/js/script.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-    <?php include './components/analyticstracking.php' ?>
+<script src="./assets/js/script.js"></script>
+
+<?php include './components/analyticstracking.php' ?>
 
 </body>
 
